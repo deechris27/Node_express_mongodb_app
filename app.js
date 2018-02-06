a336f9a3860222dc677a1bbc0e7e67b6b86b7c33
deechris27 <deechris27@dee.com>
2018-02-06 18:13:17 +0530

    Connection established, mongodb users fetched and displayed

--- app.js
+++ app.js
@@ -1,6 +1,9 @@
 var express = require('express');
 var bodyParser = require('body-parser');
 var path = require('path');
+var expressValidator = require('express-validator');
+var mongojs = require('mongojs');
+var db = mongojs('customerapp', ['users']);
 
 var app = express();
 
@@ -24,7 +27,32 @@ app.use(bodyParser.urlencoded({extended:false}));
 
 app.use(express.static(path.join(__dirname, 'public')));
 
-var users = [
+//Global variables
+app.use(function(req, res, next){
+   res.locals.errors = null;
+   next();
+});
+
+//Express validator middleware
+app.use(expressValidator({
+   errorFormatter: function(param, msg, value){
+   	var namespace = param.split('.');
+   	global = namespace.shift();
+   	formParam = global;
+
+   	while(namespace.length){
+   		formParam += '[' + namespace.shift() + ']';
+   	}
+
+   	return{
+   		param : formParam,
+   		msg: msg,
+   		value : value
+   	};
+   }
+}));
+
+ /* var users = [
  {
      id: 1,
      first_name: 'Deepak',
@@ -46,14 +74,42 @@ var users = [
       email: 'fui@gmail.com'
  }
 
-];
+]; */
 
 
 app.get('/', function(req, res){
-   res.render('index', {title: 'customers', users:users});
+	db.users.find(function(err, docs){
+         res.render('index', {title: 'customers', users:docs});
+	});
+   
 });
 
+app.post('/users/add', function(req,res){
 
+	req.checkBody('first_name','First name is required').notEmpty();
+	req.checkBody('last_name', 'Last Name is required').notEmpty();
+	req.checkBody('email', 'Email ID is required').notEmpty();
+
+     var errors = req.validationErrors();
+
+     if(errors){
+          res.render('index', {
+          	title: 'Customers',
+          	users: users,
+          	errors: errors
+          });
+     }else{
+     	var newUser = {
+       first_name: req.body.first_name,
+       last_name : req.body.last_name,
+       email_ID : req.body.email
+   }
+
+      console.log('success');
+
+     } //end of else
+
+});
 
 app.listen(3000, function(){
 	console.log('Server is now listening to port 3000');
